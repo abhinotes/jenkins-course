c3aa6fab3a560e35cb42ba3a7af0bda70176fb97
Abhishek Kumar <gabhishekkumar@outlook.com>
2022-01-10 00:58:11 +0530

    UPdated Repository & Credential Referecnes

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,22 +1,22 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/abhinotes/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Abhishek Kumar')
+            node / gitConfigEmail('gabhishekkumar@outlook.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('Nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('coolmagnet/jenkin-node-js-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('6f1ac894-c572-4559-b25d-401497cd8b39')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
